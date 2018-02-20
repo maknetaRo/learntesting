5bda3da80f83d6c3bdefd76701d2ed9aa3de52e4
Magda <makneta@gmail.com>
2018-02-20 21:52:18 +0100

    test fibonacci

---
+++ test_fibonacci.py
@@ -0,0 +1,16 @@
+import unittest
+from fibonacci import fibonacci
+
+
+class TestFibonacci(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_fibonacci_should_return_0_for_0(self):
+        self.assertEqual(fibonacci(0), 0)
+
+    def test_fibonacci_should_return_1_for_1(self):
+        self.assertEqual(fibonacci(1), 1)
+
+    def test_fibonacci_should_return_89_for_100(self):
+        self.assertEqual(fibonacci(100), 89)
